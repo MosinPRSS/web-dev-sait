e657449ef10b41567f5bcafacab7989f44e56396
MosinPRSS <ssav@sfedu.ru>
2026-09-13 14:36:54 +0300

    pr2-3 start

---
+++ pr2-3/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app: Flask = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('./pages/home.html')
+
+@app.route('/cart')
+def cart():
+    ...
+
+@app.route('/category/{name}')
+def category(): 
+    ...
+
+@app.route('/login')
+def login(): ... # not necessary for this work
+if __name__ == "__main__":
+    app.run(debug=True)
